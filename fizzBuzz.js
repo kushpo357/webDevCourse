7fef134590e7857312471db6d555559e0ea714e2
Om Patil <kushpo.030507@gmail.com>
2024-07-25 19:09:57 +0530

    fizzBuzz version 2

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -2,22 +2,24 @@ let i = 0;
 let arr = [];
 function fizzBuzz()
 {
-    i++;
-    if(i%15 == 0)
-    {
-        arr.push("fizzBuzz");
-    }
-    else if(i%5 == 0)
-    {
-        arr.push("Buzz");
-    }
-    else if(i%3 == 0)
-    {
-        arr.push("fizz");
-    }
-    else
-    {
-        arr.push(i);
+    while(i < 100){
+        i++;
+        if(i%15 == 0)
+        {
+            arr.push("fizzBuzz");
+        }
+        else if(i%5 == 0)
+        {
+            arr.push("Buzz");
+        }
+        else if(i%3 == 0)
+        {
+            arr.push("fizz");
+        }
+        else
+        {
+            arr.push(i);
+        }
     }
     console.log(arr);
 }
