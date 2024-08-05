17d4cf5c82c88ae997f3033d82361ee3f002dd9e
Om Patil <kushpo.030507@gmail.com>
2024-08-05 08:41:27 +0530

    Drum kit update

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -3,7 +3,40 @@ for(var i = 0; i < btn.length; i++)
 {
     btn[i].addEventListener("click", function () 
         {
-            alert("hello");
+            // this.style.color = "white";
+            let btnName = this.innerText;
+            let audio;
+            switch(btnName)
+            {
+                case "w":
+                    audio = new Audio("./sounds/tom-1.mp3");
+                    audio.play();
+                    break;
+                case "a":
+                    audio = new Audio("./sounds/tom-2.mp3");
+                    audio.play();
+                    break;
+                case "s":
+                    audio = new Audio("./sounds/tom-3.mp3");
+                    audio.play();
+                    break;
+                case "d":
+                    audio = new Audio("./sounds/tom-4.mp3");
+                    audio.play();
+                    break;
+                case "j":
+                    audio = new Audio("./sounds/snare.mp3");
+                    audio.play();
+                    break;
+                case "k":
+                    audio = new Audio("./sounds/crash.mp3");
+                    audio.play();
+                    break;
+                case "l":
+                    audio = new Audio("./sounds/kick-bass.mp3");
+                    audio.play();
+                    break;
+            }
         }
     );
 }
