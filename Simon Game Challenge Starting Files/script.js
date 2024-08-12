0fa16486c796965770c8f96b6a8404a26efe4623
Om Patil <kushpo.030507@gmail.com>
2024-08-12 22:36:59 +0530

    simon game changes

--- Simon Game Challenge Starting Files/script.js
+++ Simon Game Challenge Starting Files/script.js
@@ -2,67 +2,58 @@ let gameover = true;
 let colors = ["green", "red", "yellow", "blue"];
 let seq = [];
 let num = 0;
-$(document).on("keydown", function(){
-    if(gameover)
-    {
+
+$(document).on("keydown", () => {
+    if (gameover) {
         gameover = false;
-        starter(1);
+        startGame(1);
     }
 });
 
-$('.btn').click(function (){
-    if(gameover)
-        wrongBuzz();
-    else
-    {
+$('.btn').click(function () {
+    if (gameover) {
+        triggerWrongBuzz();
+    } else {
         let currbtn = colors.indexOf($(this).attr("id"));
-        if(seq[num] != currbtn)
-        {
-            wrongBuzz();
-        }
-        else
-        {
+        if (seq[num] !== currbtn) {
+            triggerWrongBuzz();
+        } else {
             pressButton(currbtn);
             num++;
-            if(num == seq.length)
-            {
+            if (num === seq.length) {
                 num = 0;
-                starter(seq.length + 1);
+                startGame(seq.length + 1);
             }
         }
     }
 });
 
-function updateSequence()
-{
-    let newval = Math.round(Math.random()*3);
-    setTimeout(() => {
-        pressButton(newval);
-        seq.push(newval);
-    }, 1500);
-    
+function updateSequence() {
+    let newval = Math.floor(Math.random() * 4);
+    seq.push(newval);
+    setTimeout(() => pressButton(newval), 1000);
 }
-function starter(level)
-{
-    $('#level-title').text("level " + level);
+
+function startGame(level) {
+    $('#level-title').text(`Level ${level}`);
     updateSequence();
 }
 
-function pressButton(ind)
-{
-    let audio = new Audio("./sounds/" + colors[ind] + ".mp3");
+function pressButton(index) {
+    let audio = new Audio(`./sounds/${colors[index]}.mp3`);
     audio.play();
-
-    $("." + colors[ind]).addClass("pressed");
-    setTimeout(() => {$("." + colors[ind]).removeClass("pressed");}, 100);
-}
     
-function wrongBuzz()
-{
+    $(`.${colors[index]}`).addClass("pressed");
+    setTimeout(() => $(`.${colors[index]}`).removeClass("pressed"), 100);
+}
+
+function triggerWrongBuzz() {
     let audio = new Audio("./sounds/wrong.mp3");
     audio.play();
+    
     $("body").addClass('game-over');
-    setTimeout(() => {$("body").removeClass("game-over")}, 100);
+    setTimeout(() => $("body").removeClass("game-over"), 100);
+    
     $('#level-title').text("Game Over, Press Any Key to Restart");
     num = 0;
     seq = [];
